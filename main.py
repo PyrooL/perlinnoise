6754822be94736d76f17431dd3f712917636c67e
Luciano <lm11887@gmail.com>
2018-05-18 23:48:40 -0700

    possibly generating a vector field, committing for the night

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import curses
 import random
+from math import pi
+import numpy as np
 
+height = 30
+width = 70
+
+# for future seeding, currently does nothing
 seed = ""
 
 while not seed.isnumeric():
@@ -10,13 +16,20 @@ while not seed.isnumeric():
         print(seed)
         break
 
+# to fill in with perlin calculations
 def noise(nx,ny):
-    # lol figure perlin out
-    return random.choice(["#","."])
+    return random.choice(["#","."]) # this is actually how noise works don't @ me
 
-height = 40
-width = 70
+# defining the vector map?
+vMap = []
+for y in range(height):
+    vMap.append([0] * width)
 
+for y in range(height):
+    for x in range(width):
+        vMap[y][x]=[random.random(), (random.randint(0,360)*np.pi/180)]
+
+# defining a 2d matrix of scalars, which then gets filled in with randomly either a # or a .
 maps = []
 
 for y in range(height):
@@ -26,11 +39,19 @@ for y in range(height):
         ny = y/height - 0.5
         maps[y][x] = noise(nx,ny)
 
+# just because i need this later and know how to do it...
+def lerp(a0, a1, w):
+    return (1.0 - w)*a0 + w*a1
+
+# curses + display the scalar map
 def main(stdscr):
     stdscr.clear()
+
+    # prints the map
     for h in range(height):
         for w in range (width):
             stdscr.addstr(h, w, maps[h][w])
+
     stdscr.refresh()
     stdscr.getkey()
 
